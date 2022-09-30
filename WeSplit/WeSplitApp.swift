8640c14f1bd987b6d08b83bf3d9989da688a3b76
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-09-30 02:23:28 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Zaid Neurothrone on 2022-09-30.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
